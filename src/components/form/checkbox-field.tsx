fdbcea57b3ed9c97c86ea4bbc409ba8b1b4674cd
yyunix <yyunix36@gmail.com>
2022-10-04 17:37:48 -0400

    :rewind: Revert checkbox-field component

--- src/components/form/checkbox-field.tsx
+++ src/components/form/checkbox-field.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as CheckIcon } from "@assets/icon-check.svg";
+
 type CheckboxFieldProps = {
   label: string;
   isChecked: boolean;
@@ -12,25 +14,33 @@ const CheckboxField = ({
   onChange,
 }: CheckboxFieldProps) => {
   return (
-    <>
+    <label className="flex items-center cursor-pointer p-3 bg-color hover:bg-main-purple/25 rounded-[4px] mb-2 last:mb-0">
       <input
         type="checkbox"
+        className="appearance-none"
         checked={isChecked}
         onChange={() => onChange(id)}
         name={label}
-        id={label}
       />
-
-      <label
-        className={`body-md p-3 bg-color rounded-[4px] mb-2 last:mb-0 hover:bg-main-purple/25  heading-color ${
-          isChecked
-            ? "before:bg-main-purple line-through text-black/50 dark:text-white/50"
-            : "before:bg-secondary-color before:border before:border-medium-gray/25"
+      <div
+        className={`w-4 h-4 rounded-sm mr-4 border border-medium-gray/25 relative flex-shrink-0 ${
+          isChecked ? "bg-main-purple border-main-purple" : "bg-white"
+        }`}
+      >
+        <CheckIcon
+          className={`${
+            isChecked ? "block" : "hidden"
+          } absolute top-[3.5px] left-[2.5px]`}
+        />
+      </div>
+      <span
+        className={`body-md heading-color ${
+          isChecked ? "line-through text-black/50 dark:text-white/50" : ""
         }`}
       >
         {label}
-      </label>
-    </>
+      </span>
+    </label>
   );
 };
 
