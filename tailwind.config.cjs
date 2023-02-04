dfbdb0f9fca90e79f8c41b117e655462394d9f30
yjcyun <yjc.yun@gmail.com>
2023-02-04 13:44:20 -0500

    add custom color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#635FC7",
+        "light-purple": "#A8A4FF",
+        red: "#EA5555",
+        "light-red": "#FF9898",
+        "light-grey": "#F4F7FD",
+        "light-blue-grey": "#E4EBFA",
+        "medium-gray": "#828FA3",
+        "blue-grey": "#3E3F4E",
+        "dark-grey": "#2B2C37",
+        "very-dark-grey": "#20212C",
+        "almost-black": "#000112",
+      },
+    },
   },
   plugins: [],
 };
